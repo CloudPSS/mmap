81899324847922d4d996f0bdfcb5bbf3e3423116
OpportunityLiu <opportunity@live.in>
2023-09-12 11:58:55 +0800

    test: add tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,12 +1,21 @@
 import { mmap } from '@cloudpss/mmap';
+import fs from 'node:fs/promises';
+import { randomBytes } from 'node:crypto';
 
-describe('should reject bad input', () => {
-    // @ts-expect-error - bad input
-    expect(() => mmap(1)).toThrowError(TypeError);
-    // @ts-expect-error - bad input
-    expect(() => mmap()).toThrowError(TypeError);
-});
+describe('mmap', () => {
+    it('should reject bad input', () => {
+        // @ts-expect-error - bad input
+        expect(() => mmap(1)).toThrowError(TypeError);
+        // @ts-expect-error - bad input
+        expect(() => mmap()).toThrowError(TypeError);
+    });
 
-describe('should work', () => {
-    expect(mmap('/dev/shm/xx')).toBeInstanceOf(Buffer);
+    it('should work', async () => {
+        const data = randomBytes(1024);
+        await fs.writeFile('/dev/shm/xx', data);
+        const mapped = mmap('/dev/shm/xx');
+        expect(mapped).toBeInstanceOf(Buffer);
+        expect(mapped).toHaveLength(1024);
+        expect(mapped).toEqual(data);
+    });
 });
